9eb44af2728db1868b1f9345a3c249cea76cabe7
Kevin Parine <kparine314@gmail.com>
2018-12-04 13:20:22 -0800

    delete posts

--- src/models/posts.js
+++ src/models/posts.js
@@ -1,11 +1,19 @@
 const db = require("../../db");
 const utils = require("../utils");
 
+
+// function getAll(entry) {
+//   models.getAll(req.params.id)
+//   .then(entry => { if(entry) return res.status(200).send(data) })
+//     .catch(next)
+// }
+
 function create(entry) {
   const errors = utils.verifyEntry(entry, "posts");
 
   if (errors.length > 0) {
-    throw { status: 400, message: "Missing: " + errors.join(", ") };
+    throw { status: 400, message: "Missing: " + errors.join(", ")
+    };
   }
 
   return db("posts")
@@ -13,4 +21,26 @@ function create(entry) {
     .returning("*");
 }
 
-module.exports = { create };
+// function update(entry) {
+//   const errors = utils.verifyEntry(entry, "posts")
+//   const body = req.body
+
+//   if (errors.length > 0) {
+//     throw { status: 400, message: "Missing " + errors.join(", ")
+//     };
+//   }
+//     return db("posts")
+//      .where({id: body.id })
+//      .update(entry)
+//      .returning("*")
+// }
+
+function remove(id) {
+  return db("posts")
+    .del()
+    .where("id", id)
+    .returning("*")
+}
+
+
+module.exports = { create, remove };
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,9 +6,8 @@ const authController = require("../controllers/auth");
 
 // router.get("/", controllers.getAll);
 // router.get("/:post_id", controllers.getOne);
-//
 router.post("/", authController.authorize, controllers.create);
-router.put("/:post_id", authController.authorize, authController.editPost, authController.test);
-// router.delete("/:post_id", authorize, permit, controllers.remove);
+// router.put("/:post_id", authController.authorize, authController.editPost, controllers.update);
+router.delete("/:post_id", authController.authorize, authController.editPost, controllers.remove);
 
 module.exports = router;
